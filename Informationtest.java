9669c262b7e74b8cd7c67c96a31acde19f90981b
Ridwanul hasan <36354494+RidwanulHasan@users.noreply.github.com>
2020-02-26 00:22:32 +0600

    README

---
+++ Informationtest.java
@@ -0,0 +1,26 @@
+package com.aiub.labexam;
+
+public class Informationtest {
+
+	public static void main(String[] args) {
+		
+		
+		Employee e1= new Employee(17358733,"Hridoy",2100);
+		
+	System.out.println(e1);
+	
+	
+	Address a1=new Address ("Dhaka","Bangladesh",e1);
+	
+	System.out.println(a1);
+	
+	
+		
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
